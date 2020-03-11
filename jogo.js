bea0c7be9e6bf9ace0478162ddff265bea245f3f
tadeifelipe <felipe_santostadei@hotmail.com>
2020-03-11 20:09:22 -0300

    change default

--- jogo.js
+++ jogo.js
@@ -1,5 +1,19 @@
 var altura  = 0;
 var largura = 0;
+var vidas = 1;
+var tempo = 15;
+var criaMosquitoTempo = 1500;
+
+var nivel = window.location.search;
+nivel = nivel.replace('?','');
+
+if(nivel === "normal"){
+    criaMosquitoTempo = 1500;
+}else if(nivel === "dificil"){
+    criaMosquitoTempo = 1000;
+}else if(nivel ==="avancado"){
+    criaMosquitoTempo = 750;
+}
 
 function ajustarTamanhoPalcoJogo(){
     //pegando as dimensões da tela 
@@ -8,6 +22,19 @@ function ajustarTamanhoPalcoJogo(){
     console.log(largura,altura);
 }
 
+var cronometro = setInterval(function(){
+    tempo--;
+
+    if(tempo < 0){
+        clearInterval(cronometro);
+        clearInterval(criaMosca);
+        window.location.href = "vitoria.html";
+    }
+    else{
+        document.getElementById('cronometro').innerHTML = tempo;  
+    }
+},1000);
+
 ajustarTamanhoPalcoJogo();
 
 function posicaoRandomica(){
@@ -15,6 +42,15 @@ function posicaoRandomica(){
 
     if(document.getElementById('mosquito')){
         document.getElementById('mosquito').remove();
+
+        //Decrementando as vidas
+        if(vidas > 3){
+            window.location.href = "fim_de_jogo.html";
+        }
+        else{
+            document.getElementById("v" + vidas).src = "imagens/coracao_vazio.png";
+            vidas++;
+        }  
     }
 
     var posicaoX = Math.floor(Math.random() * largura) - 90;
@@ -33,6 +69,9 @@ function posicaoRandomica(){
     mosquito.style.top = posicaoY + 'px';
     mosquito.style.position = 'absolute';
     mosquito.id = 'mosquito';
+    mosquito.onclick = function(){
+        this.remove();
+    }
 
     document.body.appendChild(mosquito);
 }
